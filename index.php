e508e920d42b64ba9abecf87b34c80fbdccb6602
gunok12345 <109029648+gunok12345@users.noreply.github.com>
2025-07-17 12:33:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intro PHP</title>
+</head>
+<body>
+    <h1>Welcome PHP Basic</h1>
+        <p>This is a simple PHP appication.</p>
+        <hr>
+        <h1 style="color: red;">Basic PHP Syntax</h1>
+        <pre>
+        &LT;?php
+            echo "Hello, World! ";
+        ?&GT;
+        </pre>
+        <h3> Result</h3>
+        <div style="color: blue;">
+        <?php
+        echo "hello world! <br>";
+        print "<span style='color: green;'>Nontawat Simmalaotao</span>";
+        ?></div>
+
+        <hr>
+        <h1 style="color: red;">MY MENU</h1>
+        <ul>
+            <li><a href="W01-01-phpbessic.php">PHP Basic</a></li>
+            <li><a href="w02-01-oddEvenNumber.php">odd-Even-Number</a></li>
+            <li><a href="W02-02-Grade.php">Calculate Grade</a></li>
+            <li><a href="W02-03-Product.php">Product</a></li>
+
+</ul>
+    
+</body>
+</html>
